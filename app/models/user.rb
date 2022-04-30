840e17f32e827d855ad39904ad27676fe577affb
daikiweb1221 <daiki.web1221@gmail.com>
2022-04-30 21:44:47 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   with_options presence: true do
-    validates :last_name
-    validates :first_name
+    validates :last_name, length: {maximum: 255 }
+    validates :first_name, length: {maximum: 255 }
   end
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
+
 end
