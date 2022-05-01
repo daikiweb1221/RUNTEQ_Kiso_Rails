b0485d65eeee7120112514d809c8c7d8dbe34f39
daikiweb1221 <daiki.web1221@gmail.com>
2022-05-01 19:40:38 +0900

    復習ポイント見ながら微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 root to: 'static_pages#top'
+
 get 'login', to: 'user_sessions#new'
 post 'login', to: 'user_sessions#create'
 delete 'logout', to: 'user_sessions#destroy'
+
 resources :users, only: [:new, :create]
 end
