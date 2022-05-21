3455299e43fdde112c5df9a16210865916092001
daikiweb1221 <daiki.web1221@gmail.com>
2022-05-21 21:19:06 +0900

    profilesコントローラの実装完了

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,31 @@
 class ProfilesController < ApplicationController
+  before_action :my_profile, only: [:show, :edit, :update]
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(profile_params)
+      redirect_to profile_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+    render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :avater
+    )
+  end
+
+  def my_profile
+    @user = User.find(current_user.id)
+  end
 end
