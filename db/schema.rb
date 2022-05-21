2e5d3bccda95b21d296924e2fa12c0f5c29c7279
daikiweb1221 <daiki.web1221@gmail.com>
2022-05-21 20:53:24 +0900

    avaterカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_231339) do
+ActiveRecord::Schema.define(version: 2022_05_21_114734) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -42,15 +42,7 @@ ActiveRecord::Schema.define(version: 2022_05_15_231339) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'sring' for column 'avatar'
 
 end
