8d84c8446c6052e60ce4357d62a5a4f21188bc93
daikiweb1221 <daiki.web1221@gmail.com>
2022-05-23 05:51:17 +0900

    Admin関連のコントローラ、roleカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_025525) do
+ActiveRecord::Schema.define(version: 2022_05_22_204714) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
