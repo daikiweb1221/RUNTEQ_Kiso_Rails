c18eda38530c96c2c135a20c5f6c38bbffc08779
daikiweb1221 <daiki.web1221@gmail.com>
2022-05-26 05:47:39 +0900

    admin/userのコントローラ、searchformの実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,42 @@
+class Admin::UsersController < Admin::BaseController
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+  @search = User.ransack(params[:q])
+  @users = @search.result(distinct: true).order(created_at: :desc).page(params[:page])
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_user_path, success: t('.delete')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :avatar,
+      :avatar_cache
+    )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ resources :users, only: %i[new create]
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :users, only: %i[index show edit update destroy]
   end
+
 
 end
